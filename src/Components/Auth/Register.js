d7839d8d90c154dbc77cdcd9fb6b56d2d5bf582f
Karolina Michalak <karolina0michalak@gmail.com>
2020-05-05 20:28:01 +0200

    Logging user in

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -139,7 +139,7 @@ class Register extends React.Component {
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
           <Header as="h2" icon color="orange" textAlign="center">
-            <Icon name="puzzle piece" />
+            Register
           </Header>
           <Form onSubmit={this.handleSubmit} size="large">
             <Segment stacked>
