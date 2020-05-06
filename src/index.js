f864794bef09298e73b6793401ce500c25155063
Karolina Michalak <karolina0michalak@gmail.com>
2020-05-06 21:03:24 +0200

    User data in global state

--- src/index.js
+++ src/index.js
@@ -5,13 +5,25 @@ import Login from "./Components/Auth/Login";
 import Register from "./Components/Auth/Register";
 import serviceWorker from "./registerServiceWorker";
 import "semantic-ui-css/semantic.min.css";
-import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
 import firebase from "./firebase";
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./Reducers/index";
+import {setUser} from './Actions/index'
 
+const store = createStore(rootReducer, composeWithDevTools());
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
+        this.props.setUser(user)
         this.props.history.push("/");
       }
     });
@@ -27,12 +39,14 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
 ReactDOM.render(
-  <Router>
-    <RootWithAuth />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 serviceWorker();
