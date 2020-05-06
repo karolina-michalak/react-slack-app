318b9fa19aa8b9e2ba2f154e3e88a9a14b7b2883
Karolina Michalak <karolina0michalak@gmail.com>
2020-05-06 20:49:09 +0200

    Listening to login/register onAuthStateChanged and installed redux

--- src/index.js
+++ src/index.js
@@ -4,18 +4,35 @@ import App from "./Components/App";
 import Login from "./Components/Auth/Login";
 import Register from "./Components/Auth/Register";
 import serviceWorker from "./registerServiceWorker";
-import 'semantic-ui-css/semantic.min.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
+import firebase from "./firebase";
 
-const Root = () => (
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.props.history.push("/");
+      }
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
   <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
+    <RootWithAuth />
+  </Router>,
+  document.getElementById("root")
 );
-
-ReactDOM.render(<Root />, document.getElementById("root"));
 serviceWorker();
